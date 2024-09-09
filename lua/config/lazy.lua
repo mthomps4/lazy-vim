b82d49da6d4780bb8cbd8bc98649e357248df72e
Matt Thompson <matt@echobind.com>
2024-09-09 16:03:07 -0400

    cleanup remove copilot

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -16,7 +16,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.ui.mini-starter" },
     -- CODING EXTRAS
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
-    { import = "lazyvim.plugins.extras.coding.copilot" },
+    -- { import = "lazyvim.plugins.extras.coding.copilot" },
     -- EDITOR EXTRAS
     { import = "lazyvim.plugins.extras.editor.refactoring" },
     -- Language Support
